56b1353ab2df4eb48b92a36e74327906372d8349
georgejincy <jincydon101@gmail.com>
2017-04-27 21:33:14 -0400

    Add user email and password fields to the farm table

--- models/farm.js
+++ models/farm.js
@@ -1,5 +1,23 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+
 module.exports = function(sequelize, DataTypes) {
     var farm = sequelize.define("farm", {
+    // The email cannot be null, and must be a proper email before creation
+    user_email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true
+      }
+
+    },
+    //The password cannot be null
+    password: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
     address: DataTypes.STRING,
     email: DataTypes.STRING,
     homePhone: DataTypes.STRING,
@@ -30,6 +48,22 @@ module.exports = function(sequelize, DataTypes) {
             onDelete: "cascade"
           });
         }                
+      },
+      // Creating a custom method for our farm model. This will check if an unhashed password entered by
+      // The user can be compared to the hashed password stored in our database
+      instanceMethods: {
+        validPassword: function(password){
+          return bcrypt.compareSync(password, this.password);
+        }
+
+      },
+      // Hooks are automatic methods that run during various phases of the farm Model lifecycle
+      // In this case, before a User is created, we will automatically hash their password
+      hooks: {
+        beforeCreate: function(user, options, cb) {
+          user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+          cb(null, options);
+        }
       }
     }
   );
