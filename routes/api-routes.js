7361381a71e829294fada2e293100cca19c7663e
Sarah Stevens <sssharkie1>
2017-05-04 13:20:10 -0400

    added routes to API routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -142,36 +142,60 @@ module.exports = function(app) {
   // DELETE route for deleting animals. You can access the animals id in req.params.id
   app.delete("/api/animals/:id", function(req, res) {
 
+    connection.query("DELETE * FROM animals WHERE farmId = ?;",[req.user.id], function(err, data) {
+        if (err) {
+          throw err;
+        }
+    });
   });
 
   // PUT route for updating animals. The updated animals will be available in req.body
   app.put("/api/animal", function(req, res) {
 
-  });
+    connection.query("UPDATE animals SET location = ?, AMFood = ?, PMFood = ?, AMMeds = ?, PMMeds = ?, AMNotes = ?, PMNotes = ? WHERE farmId = ?;", [req.body.location, req.body.AMFood, req.body.PMFood, req.body.AMMeds, 
+      req.body.PMMeds, req.body.AMNotes, req.body.PMNotes, req.user.id], function(err, data) {
+        if (err) {
+          throw err;
+        }
+      });
+    });
 
   // GET route for getting all of the farms
   app.get("/api/farm", function(req, res) {
 
+    connection.query("UPDATE farms SET user_email = ?, password = ?, farmName = ?, address = ?, homePhone = ?, cellPhone = ?, emergencyName = ?, emergencyNumber = ?, vetName = ?, vetNumber = ?, Notes = ?  WHERE farmId = ?;", 
+      [req.body.user_email, req.body.password, req.body.farmName, 
+      req.body.address, req.body.homePhone, req.body.cellPhone, req.body.emergencyName, 
+      req.body.emergencyNumber, req.body.vetName, req.body.vetNumber, req.body.Notes,req.user.id], function(err, data) {
+        if (err) {
+          throw err;
+        }
+    });
   });
 
-  // POST route for saving a new farms. You can create a farm using the data on req.body
-  app.post("/api/farm", function(req, res) {
 
-  });
-
-  // DELETE route for deleting farms. You can access the farm id in req.params.id
-  app.delete("/api/farm/:id", function(req, res) {
-
-  });
-
-  // PUT route for updating farm. The updated farm will be available in req.body
+  // PUT route for updating farm. 
   app.put("/api/farm", function(req, res) {
 
+    connection.query("UPDATE farms SET user_email = ?, password = ?, farmName = ?, address = ?, homePhone = ?, cellPhone = ?, emergencyName = ?, emergencyNumber = ?, vetName = ?, vetNumber = ?, Notes = ?  WHERE farmId = ?;", 
+      [req.body.user_email, req.body.password, req.body.farmName, 
+      req.body.address, req.body.homePhone, req.body.cellPhone, req.body.emergencyName, 
+      req.body.emergencyNumber, req.body.vetName, req.body.vetNumber, req.body.Notes,req.user.id], function(err, data) {
+        if (err) {
+          throw err;
+        }
+    });
   });
 
     // GET route for getting all of the invites
   app.get("/api/invite", function(req, res) {
 
+    connection.query("SELECT startDate, endDate, magicalLink FROM invites WHERE FarmID = ?;", 
+      [req.body.startDate, req.body.endDate, req.body.magicalLink, req.user.id], function(err, result) {
+        if (err) {
+          throw err;
+        }
+    });
   });
 
   // POST route for saving a new invites. You can create an invite using the data on req.body
@@ -182,20 +206,51 @@ module.exports = function(app) {
   // DELETE route for deleting invite. You can access the invite id in req.params.id
   app.delete("/api/invite/:id", function(req, res) {
 
+    connection.query("DELETE * FROM invites WHERE farmId = ?;",[req.user.id], function(err, data) {
+        if (err) {
+          throw err;
+        }
+    });
   });
 
   // PUT route for updating invite. The updated invite will be available in req.body
   app.put("/api/invite", function(req, res) {
 
+    connection.query("INSERT INTO invites SET startDate = ?, endDate = ?, magicalLink = ?, FarmID = ?, WHERE FarmId = ?;", [
+      req.body.startDate, req.body.endDate, req.body.magicalLink, req.params.id], function(err, result) {
+      if (err) {
+        throw err;
+      }
+   //res.redirect("/");
+    });
   });
 
-    // GET route for getting all of the tasks
-  app.get("/api/task", function(req, res) {
+    // GET route for getting all of the AM tasks
+  app.get("/api/AMtask", function(req, res) {
 
+    connection.query("SELECT animalType, animalName, AMFood, AMMeds, AMNotes FROM animals WHERE FarmID = ?;", 
+      [req.body.animalType, req.body.animalName, req.body.AMFood, req.body.AMMeds, req.body.AMNotes, req.user.id], function(err, result) {
+      if (err) {
+        throw err;
+      }
+    });
+  });
+
+    // GET route for getting all of the PM tasks
+  app.get("/api/PMtask", function(req, res) {
+
+    connection.query("SELECT animalType, animalName, PMFood, PMMeds, PMNotes FROM tasks WHERE FarmID = ?;", 
+      [req.body.animalType, req.body.animalName, req.body.PMFood, req.body.PMMeds, req.body.PMNotes, req.user.id], function(err, result) {
+      if (err) {
+        throw err;
+      }
+    });
   });
 
   // POST route for saving a new tasks. You can create a tasks using the data on req.body
   app.post("/api/task", function(req, res) {
+
+    
 
   });
 
@@ -207,5 +262,11 @@ module.exports = function(app) {
   // PUT route for updating task. The updated task will be available in req.body
   app.put("/api/task", function(req, res) {
 
+    connection.query("UPDATE tasks SET complete = 'TRUE' WHERE ID = ?;", [
+      req.body.startDate, req.body.endDate, req.body.magicalLink, req.params.id], function(err, result) {
+      if (err) {
+        throw err;
+      } 
+   //res.redirect("/");
+    }); 
   });
-};
