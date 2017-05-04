36d6ca435af722a14c940d2559121bef0db1ea69
georgejincy <jincydon101@gmail.com>
2017-05-04 13:36:15 -0400

    Add validations for input date

--- public/assets/javascript/schedule.js
+++ public/assets/javascript/schedule.js
@@ -34,6 +34,13 @@ $( document ).ready(function() {
     }
   } );
 
+    getAllInvites();
+
+    //AJAX get to get all invites corresponding to logged in user
+    function getAllInvites(){
+
+    }
+
 
 	//Schedule Trip - AJAX POST
     //--------------------------------------------------------
@@ -45,9 +52,39 @@ $( document ).ready(function() {
     	var inStartDate = $('#startDate').val().trim();
     	var inEndDate = $('#endDate').val().trim();
 
+    	var isValid = false;
+
+    	console.log("Start date: " + inStartDate + "EndDate: " + inEndDate);
+
     	// Validate input date fields and checkif it's in given format
-    	if(!(moment(inStartDate, 'MM-DD-YYYY', true).isValid()) && !(moment(inStartDate, 'MM-DD-YYYY', true).isValid())){
-    		
+    	if(!(moment(inStartDate, 'MM/DD/YYYY', true).isValid()) && !(moment(inEndDate, 'MM/DD/YYYY', true).isValid())){
+    		console.log("Both start date and end date are invalid.");
+    	}
+    	else if((moment(inStartDate, 'MM/DD/YYYY', true).isValid()) && !(moment(inEndDate, 'MM/DD/YYYY', true).isValid())){
+    		console.log("Please enter end date in the correct format");
+    	}
+    	else if(!(moment(inStartDate, 'MM/DD/YYYY', true).isValid()) && !(moment(inEndDate, 'MM/DD/YYYY', true).isValid())){
+    		console.log("Please enter start date in the correct format");
+    	}
+    	else{
+    		//If both dates are valid, check if EndDate is greater than start date
+    		if(moment(inEndDate).isSameOrAfter(inStartDate)){
+    			console.log("Startdate is before end date");
+    			isValid = true;
+    		}
+
+    	}
+
+    	console.log("isValid: " + isValid);
+
+    	if(isValid = true){
+
+    		//Make an AJAX request to write to DB
+    		var newSchedule = {
+    			startDate: inStartDate,
+    			endDate: inEndDate
+    		}
+
     	}
 
 
