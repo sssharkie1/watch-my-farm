91b9bbb5a4d8d5a8060129dd72fe44f323aa8a90
seamanhd <seamanhd@gmail.com>
2017-05-04 20:47:05 -0400

    React header working

---
+++ app/app.js
@@ -0,0 +1,13 @@
+
+// Main 
+// =============================================================
+
+
+
+// REACT STUFF --------------------------------
+var React = require("react");
+var ReactDOM = require("react-dom");
+
+var Header = require("./components/Header");
+
+ReactDOM.render(<Header />, document.getElementById("logo"));
